96f6f457295c15f0c42b6c1e06c46dcc8f91ab1b
pufferbommy <pufferbommy@gmail.com>
2023-02-27 16:02:08 +0700

    update words

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,15 @@ import { Link } from '@chakra-ui/react'
 function Footer() {
   return (
     <Container display="flex" py={10}>
-      <Box mr="auto">
-        Jump To{' '}
+      <Box fontSize="sm" mr="auto">
+        Jump to{' '}
         <Link color="pink.500" target="_blank" href="https://github.com/pufferbommy/sanguine">
-          Code
-        </Link>
+          this code
+        </Link>{' '}
+        on github
       </Box>
-      <Box>
-        Made with ❤️ by{' '}
+      <Box fontSize="sm">
+        Made with love by{' '}
         <Link color="pink.500" target="_blank" href="https://github.com/pufferbommy">
           @pufferbommy
         </Link>
